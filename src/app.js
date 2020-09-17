702dbb75b6cb762d93dedda8f2dca204472c6322
FarhadNawab <farhadnawab.official@gmail.com>
2020-09-17 07:14:33 +0500

    landing page desktop dark/light mode completed

--- src/app.js
+++ src/app.js
@@ -8,4 +8,21 @@ $(() => {
   $('[data-toggle-dark-mode]').on('click', () => {
     $('body').toggleClass('dark-mode');
   });
+
+  // Form validation -- Bootstrap way
+  // JavaScript for disabling form submissions if there are invalid fields
+  window.addEventListener('load', () => {
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    const forms = document.getElementsByClassName('needs-validation');
+    // Loop over them and prevent submission
+    const validation = Array.prototype.filter.call(forms, (form) => {
+      form.addEventListener('submit', (event) => {
+        if (form.checkValidity() === false) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+        form.classList.add('was-validated');
+      }, false);
+    });
+  }, false);
 });
