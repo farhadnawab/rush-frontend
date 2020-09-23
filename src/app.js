7408e0a9c561994e9503b9ba5b6f05a65b8f9003
FarhadNawab <farhadnawab.official@gmail.com>
2020-09-24 00:22:52 +0500

    added viewport height fix for ios

--- src/app.js
+++ src/app.js
@@ -9,6 +9,7 @@ $(() => {
     $('body').toggleClass('dark-mode');
   });
 
+  // Accordion
   $('.block .collapse').on('show.bs.collapse', (e) => {
     const $this = $(e.currentTarget);
 
@@ -20,6 +21,19 @@ $(() => {
     const $this = $(e.currentTarget);
 
     $this.closest('.block').removeClass('active');
+  });
+
+  // Vh height calculation for mobile
+  // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+  let vh = window.innerHeight * 0.01;
+  // Then we set the value in the --vh custom property to the root of the document
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+  // We listen to the resize event
+  window.addEventListener('resize', () => {
+    // We execute the same script as before
+    vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
   });
 
   /* $('[data-toggle="collapse"]').on('click', (e) => {
