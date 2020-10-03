860eff81596b58fe30176987d1af3a79c349771f
FarhadNawab <farhadnawab.official@gmail.com>
2020-10-03 19:25:34 +0500

    build updated

--- src/app.js
+++ src/app.js
@@ -139,7 +139,7 @@ window.addEventListener('load', () => {
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker
-      .register('/rush-frontend/serviceWorker.js')
+      .register('./serviceWorker.js')
       .then((res) => console.log('service worker registered', res))
       .catch((err) => console.log('service worker not registered', err));
   });
