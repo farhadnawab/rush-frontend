8db2ff710525542c099e5a0bad7a56f9eb3416d7
FarhadNawab <farhadnawab.official@gmail.com>
2020-10-05 04:09:43 +0500

    webpack css uri build fix

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -95,7 +95,7 @@ module.exports = function () {
           test: /\.scss$/,
           use: [
             MiniCssExtractPlugin.loader,
-            'css-loader',
+            'css-loader?url=false',
             'sass-loader',
           ],
         },
