dfb05d73cf7af515f9c2d99737efaaa30ee53e28
FarhadNawab <farhadnawab.official@gmail.com>
2020-10-16 03:00:23 +0500

    build updated

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -5,7 +5,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const MinifyPlugin = require('babel-minify-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
-const { CriticalPlugin } = require('webpack-plugin-critical');
+const HtmlCriticalWebpackPlugin = require('html-critical-webpack-plugin');
 const path = require('path');
 // We need Nodes fs module to read directory contents
 const fs = require('fs');
@@ -27,8 +27,43 @@ function generateHtmlPlugins(templateDir) {
   });
 }
 
+// Our function that generates our html plugins
+function generateCriticalHtmlPlugins(templateDir) {
+  // Read files in template directory
+  const templateFiles = fs.readdirSync(path.resolve(__dirname, templateDir));
+  return templateFiles.map((item) => {
+    // Split names and extension
+    const parts = item.split('.');
+    const name = parts[0];
+    const extension = parts[1];
+    // Create new HTMLWebpackPlugin with options
+    return new HtmlCriticalWebpackPlugin({
+      base: path.resolve(__dirname, 'docs'),
+      src: `${name}.${extension}`,
+      dest: `${name}.${extension}`,
+      inline: true,
+      minify: true,
+      extract: true,
+      penthouse: {
+        blockJSRequests: false,
+      },
+    });
+  });
+}
+
 // Call our function on our views directory.
 const htmlPlugins = generateHtmlPlugins('./src/template/view');
+const criticalHtmlPlugins = new HtmlCriticalWebpackPlugin({
+  base: path.resolve(__dirname, 'docs'),
+  src: 'one-shipment.html',
+  dest: 'one-shipment.html',
+  inline: true,
+  minify: true,
+  extract: true,
+  penthouse: {
+    blockJSRequests: false,
+  },
+});
 
 module.exports = function () {
   return {
@@ -86,16 +121,10 @@ module.exports = function () {
           },
         ],
       }),
-      new CriticalPlugin({
-        src: 'index.html',
-        inline: true,
-        minify: true,
-        dest: 'index.html',
-      }),
     ]
     // We join our htmlPlugin array to the end
     // of our webpack plugins array.
-      .concat(htmlPlugins),
+      .concat(htmlPlugins).concat(criticalHtmlPlugins),
     module: {
       rules: [
         {
